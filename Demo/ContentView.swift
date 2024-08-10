c8051d2224bde504d49807b6d1b7ba38a9a2c5bf
Zeke <zzhmiasanmia@outlook.com>
2024-08-10 14:59:06 +0800

    增加喝水功能

--- Demo/ContentView.swift
+++ Demo/ContentView.swift
@@ -8,20 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isWatered = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("demo")
+        VStack{
+            
+            Image(systemName: isWatered ? "crown.fill":"eyes")
+                .foregroundColor(.blue)
+                .font(.system(size: 150))
+                .shadow(radius: 4)
+
+            ZStack{
+                Image("cat")
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(10)
+
+            }
+            .frame(minHeight: 400)
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            
+            Divider()
+            Toggle(isOn: $isWatered) {
+                Label("今天你喝水了吗", systemImage: "drop.fill")
+            }
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
 }
 
+
+
+
+
+
+
+
+
+
 #Preview {
     ContentView()
 }
 
-//修改了文本
-//
+
+
